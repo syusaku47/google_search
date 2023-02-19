961face6fb396b68d61634fafed8692e2cc6ad92
桝田@マリエッタ <masuda.s@marietta.co.jp>
2023-02-20 00:43:34 +0900

    [Modify] 実装完成

--- source/app/Http/Controllers/GoogleController.php
+++ source/app/Http/Controllers/GoogleController.php
@@ -11,51 +11,65 @@ class GoogleController extends Controller
     {
         //取得スタート位置
         $start = $request->input('start',null);
+
+        //
+        if($start>91){
+            $start=91;
+        }elseif($start<1){
+            $start =1;
+        }
+
         //検索ワード
         $search = $request->input('search',null);
+        $total_results=null;
+        $count=null;
         //検索結果変数
         $my_arr = [];
         if (isset($request->search)) {
-//            $my_arr = GoogleService::search_google_list($search, $start);
+            $my_arr = GoogleService::search_google_list($search, $start);
 //            dd($my_arr);
-            $my_arr = [
-                'queries' => [
-                    'nextPage' => [
-                        0 => [
-                            "title" => "Google Custom Search - 桝田",
-                            "totalResults" => "7470000",
-                            "searchTerms" => "桝田",
-                            "count" => 10,
-                            "startIndex" => 11,
-                            "inputEncoding" => "utf8",
-                            "outputEncoding" => "utf8",
-                            "safe" => "off",
-                            "cx" => "e7569735cd6de4dda",
-                            "excludeTerms" => "pdf uploads",
-                        ]
-                    ],
-                ],
-                'items' => [
-                    0 => [
-                        'link' => 'https://en.wikipedia.org/wiki/Junichi_Masuda',
-                        'title' => 'Junichi Masuda - Wikipedia',
-                        'htmlSnippet' => 'Junichi <b>Masuda</b> is a Japanese video game composer, director, designer, producer, singer, programmer and trombonist, best known for his work in the Pokémon',
-                    ],
-                    1 => [
-                        'link' => 'https://masuda.fvsd.us/',
-                        'title' => 'Masuda Middle School',
-                        'htmlSnippet' => 'Kazuo <b>Masuda</b> serves 6-8th grade students and is part of Fountain Valley SD.',
-
-                    ],
-                ]
-            ];
-
+//            $my_arr = [
+//                'queries' => [
+//                    'nextPage' => [
+//                        0 => [
+//                            "title" => "Google Custom Search - 桝田",
+//                            "totalResults" => "7470000",
+//                            "searchTerms" => "桝田",
+//                            "count" => 10,
+//                            "startIndex" => 11,
+//                            "inputEncoding" => "utf8",
+//                            "outputEncoding" => "utf8",
+//                            "safe" => "off",
+//                            "cx" => "e7569735cd6de4dda",
+//                            "excludeTerms" => "pdf uploads",
+//                        ]
+//                    ],
+//                ],
+//                'items' => [
+//                    0 => [
+//                        'link' => 'https://en.wikipedia.org/wiki/Junichi_Masuda',
+//                        'title' => 'Junichi Masuda - Wikipedia',
+//                        'htmlSnippet' => 'Junichi <b>Masuda</b> is a Japanese video game composer, director, designer, producer, singer, programmer and trombonist, best known for his work in the Pokémon',
+//                    ],
+//                    1 => [
+//                        'link' => 'https://masuda.fvsd.us/',
+//                        'title' => 'Masuda Middle School',
+//                        'htmlSnippet' => 'Kazuo <b>Masuda</b> serves 6-8th grade students and is part of Fountain Valley SD.',
+//
+//                    ],
+//                ]
+//            ];
+            $queries = $my_arr['queries']['request'][0];
+            $total_results=$queries['totalResults'];
+            $count=$queries['count'];
         }
 
         return view('index', [
             'search' => $search, //検索ワード
             'my_arr' => $my_arr, //検索結果
             'start' => $start,//検索初期値
+            'total_results' => number_format($total_results),//検索取得数
+            'count' => $count,//検索初期値
         ]);
     }
 }
--- source/resources/views/index.blade.php
+++ source/resources/views/index.blade.php
@@ -20,32 +20,41 @@
         <div class="main_title">
             <h1>Google</h1>
         </div>
-        <div class="form">
-            <form method="GET" action="{{ route('google.index') }}">
-                <input class="input-area" type="search" placeholder="Googleで検索" name="search"
-                       value="@if (isset($search)) {{$search}} @endif">
-                <button type="submit">検索</button>
+        <form class="top_form" method="GET" action="{{ route('google.index') }}">
+            <input class="input-area" type="search" placeholder="Googleで検索" name="search"
+                   value="@if (isset($search)){{$search}}@endif">
+            <button type="submit">検索</button>
 
-            </form>
-        </div>
+            <input type="hidden" name="start" value="1">
+        </form>
     </div>
 </div>
 
 @else
+<div class="menu_item">
+    <div>
+        <a href="{{ route('google.index') }}" style="text-decoration:none;">Google</a>
+    </div>
+    <div class="form">
+        <form method="GET" action="{{ route('google.index') }}">
+            <input class="menu-input-area" type="search" placeholder="Googleで検索" name="search"
+                   value="@if (isset($search)){{$search}}@endif">
+            <button type="submit">検索</button>
+            <input type="hidden" name="start" value="1">
+        </form>
+
+    </div>
+</div>
 <div class="left_item">
     @if(empty($my_arr['items']))
     <p>検索結果は 0 件です。</p>
     @else
 
-    <div class="form">
-        <form method="GET" action="{{ route('google.index') }}">
-            <input class="input-area" type="search" placeholder="Googleで検索" name="search"
-                   value="@if (isset($search)) {{$search}} @endif">
-            <button type="submit">検索</button>
-        </form>
-    </div>
 
-    <p>検索結果</p>
+    <div class="search_result">
+        <span>検索結果</span>
+        <span>{{$total_results}}&nbsp;件</span>
+    </div>
     @foreach ($my_arr['items'] as $index => $value)
     <div>
         <dl>
@@ -61,21 +70,25 @@
     <div class="form">
 
         @if($start > 0)
-        <div class="inline-from">
-            <form method="GET" action="{{ route('google.index') }}">
-                <input type="hidden" name="search" value="{{$search}}">
-                <input type="hidden" name="start" value="{{$start-1}}">
-                <button type="submit">前へ</button>
-            </form>
+        <div class="button_centering">
+            <div class="inline-from">
+                <form method="GET" action="{{ route('google.index') }}">
+                    <input type="hidden" name="search" value="{{$search}}">
+                    <input type="hidden" name="start" value="{{$start-10}}">
+                    <button type="submit">前へ</button>
+                </form>
+            </div>
+            <div class="inline-from">
+                <form method="GET" action="{{ route('google.index') }}">
+                    <input type="hidden" name="search" value="{{$search}}">
+                    <input type="hidden" name="start" value="{{$start+10}}">
+                    <button type="submit">次へ</button>
+                </form>
+            </div>
         </div>
         @endif
-        <div class="inline-from">
-            <form method="GET" action="{{ route('google.index') }}">
-                <input type="hidden" name="search" value="{{$search}}">
-                <input type="hidden" name="start" value="{{$start+1}}">
-                <button type="submit">次へ</button>
-            </form>
-        </div>
+
+
     </div>
     @endif
     @endif
